4291cfa4116a198a345e8a78703778a7351545b7
caleboverman <caleboverman@gmail.com>
2025-09-27 14:45:05 -0500

    fixes

--- minitorch/modules_transfomer.py
+++ minitorch/modules_transfomer.py
@@ -125,8 +125,8 @@ class MultiHeadAttention(Module):
             mask = self.create_causal_mask(queries_len)
             scores = scores + mask
 
-        row_max = max(scores, dim=-1)                            # (batch_size, num_head, queries_len)
-        row_max = row_max.view(batch_size, num_head, queries_len, 1)  # broadcast shape
+        row_max = max(scores, dim=3)  # (B, H, T)
+        row_max = row_max.view(batch_size, num_head, queries_len, 1)
         scores = scores - row_max
         attn = softmax(scores, dim=3)
         attn = self.dropout(attn)
